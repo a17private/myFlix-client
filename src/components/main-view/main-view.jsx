5f21d36349bf3ad4a8db7b41381b1d1fdc61581a
a17labs <*a17matte@gmail.com*>
2021-09-12 18:59:18 +0100

    changes

---
+++ src/components/main-view/main-view.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { MovieCard } from '../movie-card/movie-card';
+export class MainView extends React.Component {
+
+  render() {
+    return (
+      <div className="main-view">
+        <div>Inception</div>
+        <div>The Shawshank Redemption</div>
+        <div>Gladiator</div>
+      </div>
+    );
+  }
+}
