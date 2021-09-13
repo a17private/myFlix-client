5c3c3122bd87262e17fc9ffee1d7aa1ceed8b892
a17labs <*a17matte@gmail.com*>
2021-09-13 11:44:46 +0100

    changes

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { MovieCard } from '../movie-card/movie-card';
 export class MainView extends React.Component {
 
-  render() {
-    return (
-      <div className="main-view">
-        {movies.map(movie => <MovieCard />)}
-      </div>
-    );
-  }
-}
+ render() {
+  const { movies } = this.state;
 
+  if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
+
+  return (
+    <div className="main-view">
+      {movies.map(movie => <MovieCard />)}
+    </div>
+  );
+}
