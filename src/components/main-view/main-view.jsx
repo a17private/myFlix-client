a4ec595b15e5a20daa04664d1423f0a2bbb5a7e2
a17labs <*a17matte@gmail.com*>
2021-09-15 12:20:00 +0100

    Removal of movie array

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,23 +1,35 @@
 // myFlix-client/src/main-view/main-view.jsx
 
 import React from 'react';
+import axios from 'axios';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
+
 
 
   export class MainView extends React.Component{
     constructor() {
       super();
       this.state = {
-        movies: [
-          { _id: 1, Title: 'Inception', Description: 'Inception is a 2010 science fiction action film written and directed by Christopher Nolan, who also produced the film with Emma Thomas, his wife. The film stars Leonardo DiCaprio as a professional thief who steals information by infiltrating the subconscious of his targets.', ImagePath: '...'},
-          { _id: 2, Title: 'The Shawshank Redemption', Description: 'Andy Dufresne, a successful banker, is arrested for the murders of his wife and her lover, and is sentenced to life imprisonment at the Shawshank prison. He becomes the most unconventional prisoner.', ImagePath: '...'},
-          { _id: 3, Title: 'Gladiator', Description: 'Commodus takes over power and demotes Maximus, one of the preferred generals of his father, Emperor Marcus Aurelius. As a result, Maximus is relegated to fighting till death as a gladiator.', ImagePath: "..."}
-        ],
+        movies: [],
         selectedMovie: null
       }
     }
-
+   
+   
+    componentDidMount(){
+      axios.get('https://[myflixdb17].herokuapp.com/movies')
+        .then(response => {
+          this.setState({
+            movies: response.data
+          });
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+    
+    
     setSelectedMovie(newSelectedMovie) {
       this.setState({
         selectedMovie: newSelectedMovie
