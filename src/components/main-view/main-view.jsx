27239d193a2c97fe32fa28befc0f16ea274263c9
a17labs <*a17matte@gmail.com*>
2021-09-16 10:09:57 +0100

    changes

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -2,29 +2,22 @@
 
 import React from 'react';
 import axios from 'axios';
-
-
-import { LoginView } from "../login-view/login-view";
-import { RegistrationView } from "../registration-view/registration-view";
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 
-import "./main-view.scss";
 
-
-
-  export class MainView extends React.Component {
+  export class MainView extends React.Component{
     constructor() {
       super();
-      //initial states are set to null
       this.state = {
         movies: [],
-        selectedMovie: null,
-        user: null,
-        register: null,
-      };
+        selectedMovie: null
+      }
     }
+   
    
     componentDidMount(){
       axios.get('https://myflixdb17.herokuapp.com/movies')
@@ -38,67 +31,38 @@ import "./main-view.scss";
         });
     }
     
-    
-    keypressCallback(event) {
-      console.log(event.key);
-    }
-
     componentDidMount() {
-      document.addEventListener('keypress', this.keypressCallback);
+      document.addEventListener('keypress', event => {
+        console.log(event.key);
+      });
     }
-
-  
+    
     setSelectedMovie(newSelectedMovie) {
       this.setState({
         selectedMovie: newSelectedMovie
       });
     }
-
-    onRegistration(register) {
-      this.setState({
-        register,
-      });
-    }
-  
-    /* When a user successfully logs in, this function updates the
-     `user` property in state to that *particular user*/
-    onLoggedIn(user) {
-      this.setState({
-        user,
-      });
-    }
-  
     
     render() {
-      const { movies, selectedMovie, user, register } = this.state;
-
-       /* If there is not registered user, the RegisterView is rendered. If there is a user registered,
-     the user details are *passed as a prop to the LoginView*/
-    if (!register)
-    return (
-      <RegistrationView
-        onRegistration={(register) => this.onRegistration(register)}
-      />
-    );
-
-  /* If there is no user, the LoginView is rendered. If there is a user logged in,
-   the user details are *passed as a prop to the LoginView*/
-  if (!user)
-    return <LoginView onLoggedIn={(user) => this.onLoggedIn(user)} />;
-
+      const { movies, selectedMovie } = this.state;
   
       if (movies.length === 0) return <div className="main-view" />;
   
       return (
-        <div className="main-view">
+        <Row className="main-view justify-content-md-center">
           {selectedMovie
-          ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
-          : movies.map(movie => (
-            <MovieCard key={movie._id} movie={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie) }}/>
-         ))
-        }
-      </div>
-    );
+            ? (
+              <Col md={8}>
+                <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
+              </Col>
+            )
+            : movies.map(movie => (
+              <Col md={3}>
+                <MovieCard key={movie._id} movie={movie} onMovieClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
+              </Col>
+            ))
+          }
+        </Row>
+      );
+    }
   }
-
-}
