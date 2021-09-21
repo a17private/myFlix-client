67a2e4fa47bef18490eb77f3ab7f4afea8e76cad
a17labs <*a17matte@gmail.com*>
2021-09-22 00:06:21 +0100

    Registration

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,146 +1,174 @@
-import React, { useState } from 'react';
+import React, {useState} from "react";
 import PropTypes from 'prop-types';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
-import Button from "react-bootstrap/Button";
-import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button';
 import axios from 'axios';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import NavDropdown  from 'react-bootstrap/NavDropdown';
+import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/row';
+import { Link } from "react-router-dom";
 
-
-
-import "./registration-view.scss";
-
-
+import './registration-view.scss';
 
 export function RegistrationView(props) {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [email, setEmail] = useState("");
-  const [birthdate, setBirthdate] = useState("");
 
- 
+    const [name, setName] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
+    const [birthdate, setBirthdate] = useState("");
 
- /* const [usernameError, setUsernameError] = useState({});
-  const [passwordError, setPasswordError] = useState({});
-  const [emailError, setEmailError] = useState({});
-  const [birthdateError, setBirthdateError] = useState({});*/
+    const [nameError, setNameError] = useState({});
+    const [usernameError, setUsernameError] = useState({});
+    const [passwordError, setPasswordError] = useState({});
+    const [emailError, setEmailError] = useState({});
+    const [birthdateError, setBirthdateError] = useState({});
 
-
- 
-
- 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-   /* let setisValid = formValidation();*/
-   /* if (setisValid) {*/
-      axios.post('https://myflixdb17.herokuapp.com/users', {
-        Username: username,
-        Password: password,
-        Email: email,
-        Birthday: birthdate
-      })
-        .then(response => {
-          const data = response.data;
-          console.log(data);
-          window.open('/', '_self'); // the second argument '_self' is necessary so that the page will open in the current tab
-        })
-        .catch(e => {
-          console.log('error registering the user')
-        });
-    /*};*/
-  }
-  
-  
-  
-  /*const formValidation = () => {
-    let usernameError = {};
-    let passwordError = {};
-    let emailError = {};
-    let birthdateError = {};
-    let isValid = true;
-
-    if (username.trim().length < 4) {
-      usernameError.usernameShort = "Username incorrect. Use at least 4 characters.";
-      isValid = false;
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        let setisValid = formValidation();
+        if (setisValid) {
+          axios.post('https://https://myflixdb17.herokuapp.com/users/registration', {
+            Name: name,
+            Username: username,
+            Password: password,
+            Email: email,
+            Birthdate: birthdate
+          })
+          .then(response => {
+            const data = response.data;
+            console.log(data);
+            window.open('/', '_self'); // the second argument '_self' is necessary so that the page will open in the current tab
+          })
+          .catch(e => {
+            console.log('error registering the user')
+          });
+      };
     }
-    if (password.trim().length < 5) {
-      passwordError.passwordMissing = "Password incorrect. Use at least 5 characters.";
-      isValid = false;
-    }
-    if (!(email && email.includes(".") && email.includes("@"))) {
-      emailError.emailNotEmail = "Email address incorrect.";
-      isValid = false;
-    }
-    if (birthdate === '') {
-      birthdateError.birthdateEmpty = "Please enter your birthdate.";
-      isValid = false;
-    }
-  
-    setUsernameError(usernameError);
-    setPasswordError(passwordError);
-    setEmailError(emailError);
-    setBirthdateError(birthdateError);
-    return isValid;
-  };*/
 
-  
+    const formValidation = () => {
+      let nameError = {};
+      let usernameError = {};
+      let passwordError = {};
+      let emailError = {};
+      let birthdateError = {};
+      let isValid = true;
 
+      if (name === '') {
+        nameError.nameEmpty = "Please enter your Name.";
+        isValid = false;
+      }
+      if (username.trim().length < 4) {
+        usernameError.usernameShort = "Username incorrect. Use at least 4 characters.";
+        isValid = false;
+      }
+      if (password.trim().length < 5) {
+        passwordError.passwordMissing = "Password incorrect. Use at least 5 characters.";
+        isValid = false;
+      }
+      if (!(email && email.includes(".") && email.includes("@"))) {
+        emailError.emailNotEmail = "Email address incorrect.";
+        isValid = false;
+      }
+      if (birthdate === '') {
+        birthdateError.birthdateEmpty = "Please enter your birthdate.";
+        isValid = false;
+      }
+      setNameError(nameError);
+      setUsernameError(usernameError);
+      setPasswordError(passwordError);
+      setEmailError(emailError);
+      setBirthdateError(birthdateError);
+      return isValid;
+  };
 
-  return (
-
-    /*<Navbar bg="dark" variant="dark" expand="lg">
- <Container>
-    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="me-auto">
-        <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#link">Link</Nav.Link>
-        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-          <NavDropdown.Item href="#action/3.1">User</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-         </NavDropdown>
-      </Nav>
-    </Navbar.Collapse>
-  </Container>
-</Navbar>
-*/
-
-   <Form>
+    return (
+      <Form className="register justify-content-md-center">
+        <Row>
+        <Form.Group controlId="formName">
+          <Form.Label>Name:</Form.Label>
+          <Form.Control type="text" value={name} onChange={e => setName(e.target.value)} />
+          {Object.keys(nameError).map((key) => {
+            return (
+              <div key={key}>
+                {nameError[key]}
+              </div>
+            );
+          })}
+        </Form.Group>
+      </Row>
+      <Row>
       <Form.Group controlId="formUsername">
         <Form.Label>Username:</Form.Label>
-        <Form.Control size="sm" placeholder="Name" type="text" onChange={e => setUsername(e.target.value)} />
+        <Form.Control type="text" value={username} onChange={e => setUsername(e.target.value)} />
+        {Object.keys(usernameError).map((key) => {
+          return (
+            <div key={key}>
+              {usernameError[key]}
+            </div>
+          );
+        })}
       </Form.Group>
-      <Form.Group controlId="formPassword">
-        <Form.Label>Password:</Form.Label>
-        <Form.Control size="sm" placeholder="Password" type="password" onChange={e => setPassword(e.target.value)} />
-      </Form.Group>
-      <Form.Group controlId="formEmail">
-        <Form.Label>Email:</Form.Label>
-        <Form.Control size="sm" placeholder="Email" type="email" onChange={e => setEmail(e.target.value)} />
-      </Form.Group>
-      <Form.Group controlId="formBirthdate">
-        <Form.Label>Birthdate:</Form.Label>
-        <Form.Control size="sm" placeholder="date" type="date" onChange={e => setBirthdate(e.target.value)} />
-      </Form.Group>
-        <Button variant="dark" size="sm" type="submit" onClick={handleSubmit}>
-        Join
-      </Button>
-    </Form>
+      </Row>
+      <Row>
+        <Form.Group controlId="formPassword">
+          <Form.Label>Create Password:</Form.Label>
+          <Form.Control type="password" value={password} onChange={e => setPassword(e.target.value)} />
+          {Object.keys(passwordError).map((key) => {
+            return (
+              <div key={key}>
+                {passwordError[key]}
+              </div>
+            );
+          })}
+        </Form.Group>
+      </Row>
 
+      <Row>
+        <Form.Group controlId="formEmail">
+          <Form.Label>Email:</Form.Label>
+          <Form.Control type="email" value={email} onChange={e => setEmail(e.target.value)} />
+          {Object.keys(emailError).map((key) => {
+            return (
+              <div key={key}>
+                {emailError[key]}
+              </div>
+            );
+          })}
+        </Form.Group>
+      </Row>
+      
+      <Row>
+        <Form.Group controlId="formBirthdate">
+        <Form.Label>Birthdate:</Form.Label>
+        <Form.Control type="date" value={birthdate} onChange={e => setBirthdate(e.target.value)} />
+        {Object.keys(birthdateError).map((key) => {
+          return (
+            <div key={key}>
+              {birthdateError[key]}
+            </div>
+          );
+        })}
+        </Form.Group>
+      </Row>
+
+      <span>
+        <Button type="submit" variant="dark" size="sm" onClick={handleSubmit}>Join</Button>
+        {' '}
+        <Link to="/">
+          <Button variant="dark" size="sm" type="button">Back</Button>
+        </Link>
+      </span>
+    </Form >
   );
 }
 
 RegistrationView.propTypes = {
   register: PropTypes.shape({
-    username: PropTypes.string.isRequired,
-    password: PropTypes.string.isRequired,
-    email: PropTypes.string.isRequired,
-    birthdate: PropTypes.string.isRequired,
+    Name: PropTypes.string.isRequired,
+    Username: PropTypes.string.isRequired,
+    Password: PropTypes.string.isRequired,
+    Email: PropTypes.string.isRequired,
+    BirthDate: PropTypes.string.isRequired
   }),
-  onRegistration: PropTypes.func.isRequired,
 };
+    
