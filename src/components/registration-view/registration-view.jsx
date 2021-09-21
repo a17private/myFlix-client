06407de0232a327dc3a47ceef39611f21c24cbcb
a17labs <*a17matte@gmail.com*>
2021-09-21 10:41:14 +0100

    Import statements

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -4,6 +4,8 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Button from "react-bootstrap/Button";
+import axios from 'axios';
+
 
 
 
