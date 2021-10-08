e252e57e7771073818da69d061df125e025a3ec9
a17labs <*a17matte@gmail.com*>
2021-10-08 20:58:25 +0100

    add actions

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -3,6 +3,12 @@
 export const SET_MOVIES = 'SET_MOVIES';
 export const SET_FILTER = 'SET_FILTER';
 export const SET_USER = 'SET_USER';
+export const SET_USERNAME = 'SET_USERNAME';
+export const SET_PASSWORD = 'SET_PASSWORD';
+export const SET_BIRTHDAY = 'SET_BIRTHDAY';
+export const SET_EMAIL = 'SET_EMAIL';
+export const SET_FAVORITEMOVIE = 'SET_FAVORITEMOVIE';
+
 
 export function setMovies(value) {
   return { type: SET_MOVIES, value };
@@ -14,4 +20,22 @@ export function setFilter(value) {
 
 export function setUser(value) {
   return { type: SET_USER, value };
+}
+
+export function setUsername(value) {
+  return { type: SET_USERNAME, value};
+}
+
+export function setPassword(value) {
+  return { type: SET_PASSWORD, value};
+}
+
+
+export function setBirthday(value) {
+  return { type: SET_BIRTHDAY, value};
+}
+
+
+export function setFavoritemovie(value) {
+  return { type: SET_FAVORITEMOVIE, value};
 }
